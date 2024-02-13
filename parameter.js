128e8857fd6bea93a06e872a6c48433a6e9805f3
Maasuuum <fardousalmasum09@gmail.com>
2024-02-13 11:48:02 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,5 @@
+function bringSingara(money){
+    console.log('mama singara den');
+    console.log(money) /**work as a variable name that will call if the value will declare in bringSingara(). Used no quotation because that's value will be a number type */
+}
+bringSingara(100);
