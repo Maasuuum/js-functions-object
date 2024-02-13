1c29865d764718b836a3f642ae5e2a798ee1c79b
Maasuuum <fardousalmasum09@gmail.com>
2024-02-13 11:37:33 +0600

    function started

---
+++ function.js
@@ -0,0 +1,12 @@
+// function declaration
+function givenTask(){
+    console.log('Stand up from bed');
+    console.log('Move towards the switch');
+    console.log('Turn on the fan switch');
+}
+// call the function
+givenTask();
+// act like as a loop.example under
+givenTask();
+givenTask();
+givenTask();
