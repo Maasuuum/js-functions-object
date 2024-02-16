663bf52c1de38ea79bab626f4c82557349a4aa6f
Maasuuum <fardousalmasum09@gmail.com>
2024-02-16 12:07:56 +0600

    return statement

--- parameter.js
+++ parameter.js
@@ -1,5 +1,25 @@
+// function bringSingara(money){
+//     console.log('mama singara den');
+//     console.log(money) /**work as a variable name that will call if the value will declare in bringSingara(). Used no quotation because that's value will be a number type */
+// }
+// bringSingara(100);
+
 function bringSingara(money){
-    console.log('mama singara den');
-    console.log(money) /**work as a variable name that will call if the value will declare in bringSingara(). Used no quotation because that's value will be a number type */
+    console.log('taka disen: ', money);
+    console.log('ei nen singara');
 }
-bringSingara(100);
+var taka = 300;
+// bringSingara(taka);
+
+// multiple parameters
+function add(num1, num2){
+    console.log('going to add:', num1, num2);
+}
+// add(125, 96);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log('Total is: ', sum);
+}
+sum(10, 5, 18, 17, 39);
---
+++ return.js
@@ -0,0 +1,20 @@
+// function add(num1, num2){
+//     console.log(num1, num2);
+//     var sum = num1 + num2;
+//     // console.log(sum);
+//     return sum;
+//     // return;
+// }
+// add(105, 17);
+// var total = add(80, 20);
+// console.log('total', total);
+
+function bringSingara(money){
+    var singaraPrice = 10;
+    var quantity = money / singaraPrice;
+    console.log(quantity);
+    // return quantity;
+}
+bringSingara(200);
+// var singaras = bringSingara(200);
+// console.log('Eating Singaras: ', singaras);
